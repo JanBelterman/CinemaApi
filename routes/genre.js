fe451b2a431c5e53511ef9ce343fa1750c5923c3
Jan Belterman <jjabelte@avans.nl>
2018-06-09 09:27:32 +0200

    Genre delete endpoint

--- routes/genre.js
+++ routes/genre.js
@@ -5,7 +5,7 @@ const { validate } = require('../models/genre');
 
 const router = express.Router();
 
-// Route to get all genres
+// Endpoint to get all genres
 router.get('/', auth, (req, res) => {
 
     // Query datbase for all genres
@@ -19,7 +19,7 @@ router.get('/', auth, (req, res) => {
 
 });
 
-// Route to post a new genre
+// Endpoint to post a new genre
 router.post('/', auth, (req, res) => {
 
     // Validate client input
@@ -42,12 +42,12 @@ router.post('/', auth, (req, res) => {
         // Return response containing the created genre
         res.status(200).send(genre);
 
-    })
+    });
 
 });
 
-// Route to put a new genre
-router.put('/:id', auth, (req, res) => {
+// Endpoint to put a new genre
+router.put('/:ID', auth, (req, res) => {
 
     // Validate client input
     const { error } = validate(req.body);
@@ -55,7 +55,7 @@ router.put('/:id', auth, (req, res) => {
 
     // Update genre in database
     let genre = {
-        ID: req.params.id,
+        ID: req.params.ID,
         title: req.body.title
     }
     let query = database.query(`UPDATE genre SET title = '${genre.title}' WHERE ID = ${genre.ID}`, (error, result) => {
@@ -63,6 +63,26 @@ router.put('/:id', auth, (req, res) => {
 
         // Return response containing the updated genre
         res.status(200).send(genre);
+
+    });
+
+});
+
+// Endpoint to delete a genre
+router.delete('/:ID', auth, (req, res) => {
+
+    // Check if genre exists
+    database.query(`SELECT * FROM genre WHERE ID = ${req.params.ID}`, (error, result) => {
+        if (error) console.log(error);
+
+        if (!result[0]) return res.status(404).send(`Request terminated: no genre found with ID: ${req.params.ID}`);
+
+        database.query(`DELETE FROM genre WHERE ID = ${req.params.ID}`, (error) => {
+            if (error) console.log(error);
+
+            res.status(200).send(result[0]);
+
+        });
 
     });
 
