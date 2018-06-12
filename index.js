e9bfa4afde33ef22b415a8ddee187a9d2aaec9ff
Jan Belterman <jjabelte@avans.nl>
2018-06-12 22:14:09 +0200

    Bugfixes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use('/api/manager', manager);
 app.use('/api/genre', genre);
 app.use('/api/hall', hall);
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}..`)
 });
