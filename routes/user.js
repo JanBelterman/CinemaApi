3da57b0a035b6baa592c79e7fd80113820e49144
Jan Belterman <jjabelte@avans.nl>
2018-08-30 23:05:44 +0200

    Comments fixed

--- routes/user.js
+++ routes/user.js
@@ -10,34 +10,34 @@ const router = express.Router();
 // Endpoint for loggin in
 router.post('/login', (req, res) => {
 
-    // Validate raw input
+    // Validate client input
     const { error } = validate(req.body);
     if (error) return res.status(412).send(error.details[0].message);
 
-    // Get user
+    // Get user from request
     let user = {
         username: req.body.username,
         password: req.body.password
     };
 
-    // Query user
+    // Get user from database
     database.query(`SELECT * FROM user WHERE username = '${user.username}'`, (error, result) => {
         if (error) console.log(error);
 
-        // Check if user exists
+        // If no user is found, return response
         if (result.length == 0) return res.status(401).send('Access denied. Not a valid user');
 
         // Check if password is correct
         if (user.password !== result[0].password) return res.status(401).send('Access denied. Password is incorrect');
 
-        // extend user with token
+        // User format to return
         user = {
             username: req.body.username,
             password: req.body.password,
             token: jwt.sign({ ID: result[0].ID}, 'SeCrEtJsOnWeBtOkEn')
         };
 
-        // Send succesfull login response
+        // Login successful, return user to client
         res.status(200).send(user);
 
     });
@@ -51,17 +51,17 @@ router.post('/register', (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(412).send(error.details[0].message);
 
-    // Get user
+    // Get user from client
     let user = {
         username: req.body.username,
         password: req.body.password
     };
 
-    // Query user
+    // Get user from databse
     database.query(`SELECT * FROM user WHERE username = '${req.body.username}'`, (error, result) => {
         if (error) console.log(error);
 
-        // Check if user exists
+        // If user aleady exists, return response
         if (result.length > 0) return res.status(405).send('Cannot register: User already exists');
 
         // Create user
