2aa786ace2f37f315af1bf71d1b861c7b1656b0d
Jan Belterman <jjabelte@avans.nl>
2018-11-06 20:24:18 +0100

    Removed sensitive data

--- database.js
+++ database.js
@@ -1,19 +1,16 @@
 const mysql = require('mysql');
 
 let database = mysql.createConnection({
-    host: process.env.DATABASE_HOST || 'localhost',
-    user: process.env.DATABASE_USER || 'cinema_app_user',
-    password: process.env.DATABASE_PASSWORD || 'IyIdBqs6ACmZhjVW',
-    database: process.env.DATABASE_NAME || 'Cinema',
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     insecureAuth: true
 });
 
 database.connect((error) => {
-
     if (error) return console.log(error);
-
     console.log('Connected to database');
-
 });
 
 module.exports = database;
